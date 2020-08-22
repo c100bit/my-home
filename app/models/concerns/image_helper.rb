c8c5b223ca4518b2b350eab08f19ae6cef629413
Иван Окулов <i.okulov@fun-box.ru>
2020-08-22 18:58:31 +0300

    fix seeds

--- app/models/concerns/image_helper.rb
+++ app/models/concerns/image_helper.rb
@@ -7,11 +7,11 @@ module ImageHelper
   included do
     has_one_attached :image
 
-    validates :image, attached: true,
-                      content_type: ['image/png', 'image/jpg', 'image/jpeg'],
-                      dimension: { width: { in: 50..10_000 },
-                                   height: { in: 50..10_000 } },
-                      size: { less_than: 1.megabyte }
+    validates :image,
+              content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+              dimension: { width: { in: 50..10_000 },
+                           height: { in: 50..10_000 } },
+              size: { less_than: 1.megabyte }
   end
 
   def image_url
